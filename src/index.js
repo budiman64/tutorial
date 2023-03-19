7356a9249d8a3671641c31e10b3f2a811ebd7796
B.Harith <48582284+budiman64@users.noreply.github.com>
2023-03-19 12:03:37 +0800

    Finish initial config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const Greeting = () => {
-    return <h2>My First Component</h2>;
+  return <h2>My First Component</h2>
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting/>);
+root.render(<Greeting />)
